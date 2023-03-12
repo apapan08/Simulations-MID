bc79db710d6a3cd81d333dcfe3fb10e9e933e989
Angelos Papanastasiou <aggelospapanastasiou905@gmail.com>
2023-03-12 18:20:45 +0200

    added seed git push origin master

--- Create_signals.R
+++ Create_signals.R
@@ -39,7 +39,7 @@ for (i in 1:nrow(simulation_setup)){
   timeserie_list[[paste0(
     as.character(dime),"_",as.character(n_changepoints),"_",as.character(sparsity)
   )]]=list()
-  
+  set.seed(27)
   for (j in 1:m){
     matrix_temp = random_matrix(d=dime,n=length_of_timeserie,number_of_changepoints = n_changepoints,
                                     sparsity = sparsity,a_uniform = 2, b_uniform = 2, noise_distr = "Gaussian")
